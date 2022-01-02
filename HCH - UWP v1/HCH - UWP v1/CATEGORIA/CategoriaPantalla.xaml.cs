4a1a146100a1bced4c1964354e2060bc3a367601
Danny Benjamin Huacon Zapata <danny.huacon@udla.edu.ec>
2022-01-02 13:16:47 -0500

    carpeta y categoria

---
+++ HCH - UWP v1/HCH - UWP v1/CATEGORIA/CategoriaPantalla.xaml.cs
@@ -0,0 +1,108 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using System.Net.Http;
+using System.Net;
+using HCH___UWP_v1.Classes;
+using Newtonsoft.Json;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace HCH___UWP_v1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CategoriaPantalla : Page
+    { 
+
+         public static string CATEGORIAUrl = "https://localhost:44399/api/categoria";
+    
+        public CategoriaPantalla()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MainPageFO_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void CarritoFO_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(CarritoPantalla));
+        }
+
+        //private void CategoriaFO_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.Frame.Navigate(typeof(CategoriaPantalla));
+        //}
+
+        private void CompraFO_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(CompraPantalla));
+        }
+
+        private void DetalleCompraFO_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(DetalleCompraPantalla));
+        }
+
+        private void MarcaFO_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MarcaPantalla));
+        }
+
+        private void ProductoFO_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ProductoPantalla));
+        }
+
+        private void UsuarioFO_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(UsuarioPantalla));
+        }
+       protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+           HttpClient client = new HttpClient();
+            var JsonReponse = await client.GetStringAsync("https://localhost:44399/api/categoria");
+            var CATEGORIAResult = JsonConvert.DeserializeObject<List<CATEGORIA>>(JsonReponse);
+            ListaCategoria.ItemsSource = CATEGORIAResult;
+        }
+
+
+
+
+
+    //      private async void Button_Click(object sender, RoutedEventArgs e)
+       //    {
+       //          var httpHandler = new HttpClientHandler();
+       //          var request = new HttpRequestMessage();
+       //          request.RequestUri = new Uri(CATEGORIAUrl);
+       //          request.Method = HttpMethod.Get;
+      //      request.Headers.Add("Accept", "application/json");
+      //      var client = new HttpClient(httpHandler);
+
+     //        HttpResponseMessage response = await client.SendAsync(request);
+     //       if (response.StatusCode == HttpStatusCode.OK)
+      //      {
+      //          string API1 = await response.Content.ReadAsStringAsync();
+      //          var resultado = JsonConvert.DeserializeObject<List<CATEGORIA>>(API1);
+      //          ListaCategoria.ItemsSource = resultado;
+
+      //      }
+        }
+    }
+    
---
+++ HCH - UWP v1/HCH - UWP v1/CATEGORIA/EditarCategoria.xaml.cs
@@ -0,0 +1,61 @@
+﻿using HCH___UWP_v1.Classes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace HCH___UWP_v1
+{
+    /// <summary>
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class EditarCategoria : Page
+    {
+        private CATEGORIA categoria;
+    
+        public EditarCategoria()
+        {
+            this.InitializeComponent();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            categoria = e.Parameter as CATEGORIA;
+
+            IDDescripcionTB.Text = categoria.Descripcion;
+
+        }
+        private async void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new HttpClient();
+            await client.DeleteAsync("https://localhost:44399/api/categoria/" + categoria.IdCategoria);
+            Frame.GoBack();
+        }
+        private async void AppBarButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            var client = new HttpClient();
+
+            categoria.Descripcion = IDDescripcionTB.Text;
+            categoria.PRODUCTO = null;
+            var CATEGORIAJson = JsonConvert.SerializeObject(categoria);
+            var HttpContent = new StringContent(CATEGORIAJson);
+            HttpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("Application/json");
+
+            await client.PutAsync("https://localhost:44399/api/Categoria/" + categoria.IdCategoria, HttpContent);
+            Frame.GoBack();
+        }
+        }
+}
