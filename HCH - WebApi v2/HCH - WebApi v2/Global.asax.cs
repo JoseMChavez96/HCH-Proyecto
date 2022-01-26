f5080cbccec98adb175e47419482672edc09280c
Danny Benjamin Huacon Zapata <danny.huacon@udla.edu.ec>
2022-01-26 18:49:40 -0500

    producto

--- HCH - WebApi v2/HCH - WebApi v2/Global.asax.cs
+++ HCH - WebApi v2/HCH - WebApi v2/Global.asax.cs
@@ -18,6 +18,7 @@ namespace HCH___WebApi_v2
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
--- HCH - WebApi v2/HCH - WebApi v2/Models/PRODUCTO.cs
+++ HCH - WebApi v2/HCH - WebApi v2/Models/PRODUCTO.cs
@@ -1,5 +1,6 @@
 namespace HCH___WebApi_v2.Models
 {
+    using Newtonsoft.Json;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
@@ -12,6 +13,7 @@ namespace HCH___WebApi_v2.Models
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public PRODUCTO()
         {
+
             CARRITO = new HashSet<CARRITO>();
             DETALLE_COMPRA = new HashSet<DETALLE_COMPRA>();
         }
@@ -41,13 +43,16 @@ namespace HCH___WebApi_v2.Models
         public DateTime? FechaRegistro { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        [JsonIgnore]
         public virtual ICollection<CARRITO> CARRITO { get; set; }
-
+        [JsonIgnore]
         public virtual CATEGORIA CATEGORIA { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        [JsonIgnore]
         public virtual ICollection<DETALLE_COMPRA> DETALLE_COMPRA { get; set; }
 
+        [JsonIgnore]
         public virtual MARCA MARCA { get; set; }
     }
 }
